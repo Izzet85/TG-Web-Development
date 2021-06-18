6f493279b3757ad8b4d681068881ab4a08be35f7
Izzet <izzetates85@gmail.com>
2021-06-18 15:57:25 +0200

    quiz-opdracht-week-7

---
+++ JavaScript/quiz-opdracht-week-7/quiz.js
@@ -0,0 +1,24 @@
+// create elements
+
+let divContainer = document.querySelector('button')
+divContainer.
+
+var button = document.createElement('button')
+button.innerHTML = 'Start'
+
+
+var div = document.getElementsByTagName('div')[0]
+div.appendChild(button)
+
+
+button.addEventListener('click', function () {
+  div.classList.add('hide')
+
+})
+
+// manipulate elements
+
+// styling elements via addClasses
+
+// 'element.classList.add("my-class");'
+// element.classList.remove('my-class')
