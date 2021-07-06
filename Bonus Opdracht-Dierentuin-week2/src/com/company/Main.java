094c98d89229580bd7c948c9269a64c9a47190bf
Izzet <izzetates85@gmail.com>
2021-07-06 16:26:14 +0200

    initial commit bonus opdracht dierentuin week2

---
+++ Bonus Opdracht-Dierentuin-week2/src/com/company/Main.java
@@ -0,0 +1,49 @@
+package com.company;
+
+public class Main {
+
+    class Zoo {
+
+
+
+        public void add(){
+
+        }
+        public void remove(){
+
+        }
+
+        public void update(){
+
+        }
+
+
+    }
+
+    class Animal{
+
+//        public static void name;
+
+
+
+    }
+
+
+    class Bird extends Animal{
+
+
+    }
+
+    class reptile extends Animal{
+
+
+    }
+    class Cat extends Animal{
+
+
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
